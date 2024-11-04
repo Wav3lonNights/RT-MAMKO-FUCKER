1f314f0ec57e33c14d0f90af872f3e75c6d98c12
wav3lon <149956826+Wav3lonNights@users.noreply.github.com>
2024-11-04 17:23:44 +0500

    Delete RT_MAMKO_FUCKER directory

--- RT_MAMKO_FUCKER/wp.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-wp id >%~dp0wpid.txt
-for /F %%i in (%~dp0wpid.txt) do set wpid=%%i && del %~dp0wpid.txt
-start mpv 1.mp4 --wid=%wpid% --loop=inf --player-operation-mode=pseudo-gui --force-window=yes
